0c6d80650ca16ba8cab1ae110173918cad6e2062
Roberto Kondo <robertokondo8496@gmail.com>
2021-11-07 00:58:12 +0000

    Cria component naipes

---
+++ src/app/models/naipes.spec.ts
@@ -0,0 +1,25 @@
+import { Naipes } from './naipes';
+
+describe('Naipes', () => {
+
+  let paus: Naipes;
+  let espadas: Naipes;
+  let ouros: Naipes;
+  let copas: Naipes;
+
+  beforeEach(() => {
+    paus = new Naipes(
+      'Paus',
+    );
+    espadas = new Naipes(
+      'Espadas',
+    );
+    ouros = new Naipes(
+      'Espadas',
+    );
+    copas = new Naipes(
+      'Espadas',
+    );    
+  })
+
+});
